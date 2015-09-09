6c3a986f4541fab65c023fa5630555061117f7ec
group 14 <sergicarol35@gmail.com>
2015-09-09 16:39:01 +0200

    borrat coses inutils

--- H2_Group_14/app/src/main/gen/com/example/group14/h1_group_14/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.example.group14.h1_group_14;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
