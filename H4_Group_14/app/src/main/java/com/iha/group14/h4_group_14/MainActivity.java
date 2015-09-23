05803c8bb79bfd8045d434a89f9bdee36cb93bcd
group 14 <sergicarol35@gmail.com>
2015-09-23 17:38:59 +0200

    Bounded services

--- H4_Group_14/app/src/main/java/com/iha/group14/h4_group_14/MainActivity.java
+++ H4_Group_14/app/src/main/java/com/iha/group14/h4_group_14/MainActivity.java
@@ -1,11 +1,43 @@
+/*
+REFERENCES: http://www.techotopia.com/index.php/Android_Local_Bound_Services_%E2%80%93_A_Worked_Example
+http://developer.android.com/guide/components/bound-services.html
+ */
+
 package com.iha.group14.h4_group_14;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    // call functions from service usuing data.function_name()
+    Weather_Data data;
+    boolean is_bound = false;
+
+    @Override
+
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, Weather_Data.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (is_bound) {
+            unbindService(mConnection);
+            is_bound = false;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +66,24 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    // Binding function
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            Weather_Data.LocalBinder binder = (Weather_Data.LocalBinder) service;
+            data = binder.getService();
+            is_bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            is_bound = false;
+        }
+    };
+
 }
--- H4_Group_14/app/src/main/java/com/iha/group14/h4_group_14/Weather_Data.java
+++ H4_Group_14/app/src/main/java/com/iha/group14/h4_group_14/Weather_Data.java
@@ -1,16 +1,33 @@
+/*
+REFERENCE http://developer.android.com/guide/components/bound-services.html
+ */
+
 package com.iha.group14.h4_group_14;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class Weather_Data extends Service {
-    public Weather_Data() {
+
+    private final IBinder mBinder = new LocalBinder();
+
+    public class LocalBinder extends Binder {
+        Weather_Data getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return Weather_Data.this;
+        }
     }
+
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
+
+    public Weather_Data() {
+
+    }
+
 }
